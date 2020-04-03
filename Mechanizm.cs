8e063b1a484e2fb589d76ed3342a342a24481f7c
Gefradus <Fader12345@gmail.com>
2020-04-03 16:23:09 +0200

    Dodaj pliki projektów.

---
+++ Mechanizm.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Wyswietlacz7seg
+{
+    [Serializable]
+    public class Mechanizm
+    {
+        private bool[] stan;
+
+        public Mechanizm()
+        {
+            stan = new bool[7];
+            WyzerujStany();
+        }
+
+        public void WyzerujStany()
+        {
+            for (int i = 0; i < stan.Length; i++)
+            {
+                stan[i] = false;
+            }
+        }
+
+        public void UstawStan(char podanyStan)
+        {
+            if (podanyStan == '0') Ustaw0();
+            if (podanyStan == '1') Ustaw1();
+            if (podanyStan == '2') Ustaw2();
+            if (podanyStan == '3') Ustaw3();
+            if (podanyStan == '4') Ustaw4();
+            if (podanyStan == '5') Ustaw5();
+            if (podanyStan == '6') Ustaw6();
+            if (podanyStan == '7') Ustaw7();
+            if (podanyStan == '8') Ustaw8();
+            if (podanyStan == '9') Ustaw9();
+            if (podanyStan == 'a' || podanyStan == 'A') UstawA();
+            if (podanyStan == 'b' || podanyStan == 'B') UstawB();
+            if (podanyStan == 'c' || podanyStan == 'C') UstawC();
+            if (podanyStan == 'd' || podanyStan == 'D') UstawD();
+            if (podanyStan == 'e' || podanyStan == 'E') UstawE();
+            if (podanyStan == 'f' || podanyStan == 'F') UstawF();
+        }
+
+        public bool[] GetStan()
+        {
+            return stan;
+        }
+
+
+        public void SetStan(bool[] podanyStan)
+        {
+            stan = podanyStan;
+        }
+
+        public void Ustaw1()
+        {
+            stan = new bool[] { false, true, true, false, false, false, false };
+        }
+        public void Ustaw2()
+        {
+            stan = new bool[] { true, true, false, true, true, false, true };
+        }
+        public void Ustaw3()
+        {
+            stan = new bool[] { true, true, true, true, false, false, true };
+        }
+        public void Ustaw4()
+        {
+            stan = new bool[] { false, true, true, false, false, true, true };
+        }
+        public void Ustaw5()
+        {
+            stan = new bool[] { true, false, true, true, false, true, true };
+        }
+        public void Ustaw6()
+        {
+            stan = new bool[] { true, false, true, true, true, true, true };
+        }
+        public void Ustaw7()
+        {
+            stan = new bool[] { true, true, true, false, false, false, false };
+        }
+        public void Ustaw8()
+        {
+            stan = new bool[] { true, true, true, true, true, true, true };
+        }
+        public void Ustaw9()
+        {
+            stan = new bool[] { true, true, true, true, false, true, true };
+        }
+        public void Ustaw0()
+        {
+            stan = new bool[] { true, true, true, true, true, true, false };
+        }
+        public void UstawA()
+        {
+            stan = new bool[] { true, true, true, false, true, true, true };
+        }
+        public void UstawB() { 
+            stan = new bool[] { false, false, true, true, true, true, true };
+        }
+        public void UstawC()
+        {
+            stan = new bool[] { true, false, false, true, true, true, false };
+        }
+        public void UstawD()
+        {
+            stan = new bool[] { false, true, true, true, true, false, true };
+        }
+        public void UstawE()
+        {
+            stan = new bool[] { true, false, false, true, true, true, true };
+        }
+        public void UstawF()
+        {
+            stan = new bool[] { true, false, false, false, true, true, true };
+        }
+
+
+
+        public void Serialize(String path)
+        {
+            FileStream fs = new FileStream(path, FileMode.Create);
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            try
+            {
+                formatter.Serialize(fs, this);
+            }
+            catch (SerializationException e)
+            {
+                Console.WriteLine("Failed to serialize. Reason: " + e.Message);
+                throw;
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+
+        public Mechanizm Deserialize(String path)
+        {
+            try
+            {
+                FileStream fs = new FileStream(path, FileMode.Open);
+                BinaryFormatter formatter = new BinaryFormatter();
+                Mechanizm mechanizm = (Mechanizm)formatter.Deserialize(fs);
+                return mechanizm;
+            }
+            catch
+            {
+                return new Mechanizm();
+            }
+
+        }
+    }
+}
+
---
+++ Wyswietlacz.cs
@@ -0,0 +1,114 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace Wyswietlacz7seg
+{
+    public class Wyswietlacz : Panel
+    {
+
+        private Mechanizm mechanizm;
+        private Color kolorWyswietlacza, kolorNapisu;
+
+        public Wyswietlacz()
+        {
+            mechanizm = new Mechanizm();
+            UstawieniaDomyslne();
+        }
+
+        private void UstawieniaDomyslne()
+        {
+            //w razie gdyby użytkownik nie zmienił ustawień komponentu ustawienia domyślne
+            kolorWyswietlacza = Color.Black;
+            kolorNapisu = Color.Red;
+            BackColor = kolorWyswietlacza;
+            Size = new Size(100, 200);   
+        }
+
+        public void WylaczWyswietlacz()
+        {
+            mechanizm.WyzerujStany();
+            PaintComponent(CreateGraphics());
+        }
+
+        public void Ustaw(char stan)
+        {
+            mechanizm.UstawStan(stan);
+            PaintComponent(CreateGraphics());
+        }
+
+        public void PaintComponent(Graphics g)
+        {
+           
+            SolidBrush brush = new SolidBrush(kolorNapisu);
+            g.FillRectangle(new SolidBrush(kolorWyswietlacza), 0, 0, 100, 200);
+
+            // segment a
+            if (mechanizm.GetStan()[0])
+            {
+                g.FillRectangle(brush, new Rectangle(0, 0, 100, 8));
+            }
+            // segment b
+            if (mechanizm.GetStan()[1])
+            {
+                g.FillRectangle(brush, new Rectangle(92, 0, 8, 100));
+            }
+            // segment c
+            if (mechanizm.GetStan()[2])
+            {
+                g.FillRectangle(brush, new Rectangle(92, 100, 8, 100));
+            }
+            // segment d
+            if (mechanizm.GetStan()[3])
+            {
+                g.FillRectangle(brush, new Rectangle(0, 192, 100, 8));
+            }
+            // segment e
+            if (mechanizm.GetStan()[4])
+            {
+                g.FillRectangle(brush, new Rectangle(0, 100, 8, 100));
+            }
+            // segment f
+            if (mechanizm.GetStan()[5])
+            {
+                g.FillRectangle(brush, new Rectangle(0, 0, 8, 100));
+            }
+            // segment g
+            if (mechanizm.GetStan()[6])
+            {
+                g.FillRectangle(brush, new Rectangle(0, 100, 100, 8));
+            }
+        }
+
+        public Mechanizm GetMechanizmWYswietlacza()
+        {
+            return mechanizm;
+        }
+
+        public void SetMechanizmWYswietlacza(Mechanizm mechanizmWYswietlacza)
+        {
+            mechanizm = mechanizmWYswietlacza;
+        }
+
+        public Color GetKolorWyswietlacza()
+        {
+            return kolorWyswietlacza;
+        }
+
+        public void SetKolorWyswietlacza(Color kolorWyswietlacza)
+        {
+            this.kolorWyswietlacza = kolorWyswietlacza;
+        }
+
+        public Color GetKolorNapisu()
+        {
+            return kolorNapisu;
+        }
+
+        public void SetKolorNapisu(Color kolorNapisu)
+        {
+            this.kolorNapisu = kolorNapisu;
+        }
+
+    }
+}
